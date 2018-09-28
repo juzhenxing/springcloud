768f6fbd69a933ea811575805f9c00b576926af2
juzhenxing <juzhenxing1@banggood.com>
2018-09-28 18:10:06 +0800

    2018年9月28日18:09:42 修改通过feign上传文件

--- eureka-feign-upload-client/src/main/java/com/example/eurekafeignuploadclient/EurekaFeignUploadClientApplication.java
+++ eureka-feign-upload-client/src/main/java/com/example/eurekafeignuploadclient/EurekaFeignUploadClientApplication.java
@@ -2,10 +2,12 @@ package com.example.eurekafeignuploadclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
 
 @EnableFeignClients
+@EnableEurekaClient
 @SpringBootApplication
 public class EurekaFeignUploadClientApplication {
 
--- eureka-feign-upload-client/src/test/java/com/example/eurekafeignuploadclient/EurekaFeignUploadClientApplicationTests.java
+++ eureka-feign-upload-client/src/test/java/com/example/eurekafeignuploadclient/EurekaFeignUploadClientApplicationTests.java
@@ -1,16 +1,51 @@
 package com.example.eurekafeignuploadclient;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EurekaFeignUploadClientApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Autowired
+	private UploadService uploadService;
+
+	@Test
+	@SneakyThrows
+	public void testFileUpload() {
+		File file = new File("d:/新建文本文档.txt");
+		FileItem diskFileItem = new DiskFileItemFactory().createItem("file", MediaType.TEXT_PLAIN_VALUE,  true, file.getName());
+		try(InputStream inputStream = new FileInputStream(file);
+			OutputStream outputStream = diskFileItem.getOutputStream();
+		){
+			IOUtils.copy(inputStream, outputStream);
+		}catch (Exception e){
+			throw new IllegalArgumentException("e: [{}]", e);
+		}
+		MultipartFile multipartFile = new CommonsMultipartFile(diskFileItem);
+		log.info(uploadService.handleFileUpload(multipartFile));
 	}
 
 }
